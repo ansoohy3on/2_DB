8037a59580d8f31d0acd36060abc33cd4c2a06e5
ash <dkstngus0615@gmail.com>
2023-07-28 18:24:14 +0900

    0728 커밋

--- ash 커뮤니티 테이블 생성.sql
+++ ash 커뮤니티 테이블 생성.sql
@@ -357,6 +357,32 @@ AND IMG_LEVEL = ?;
 -- 게시글 이미지 삭제
 DELETE FROM BOARD_IMG
 WHERE BOARD_NO = ?
-AND IMG_LEVEL IN (
+AND IMG_LEVEL IN (1,2);
 
 -- pstmt.setString(2, deleteList)
+
+-- 2번 게시판에서 제목에 "50"이 포함된 게시글 수 조회
+SELECT COUNT(*) FROM BOARD
+JOIN MEMBER USING(MEMBER_NO)
+WHERE BOARD_ST = 'N'
+AND BOARD_CD = 2
+-- AND BOARD_TITLE LIKE '%50%'; -- 제목
+-- AND BOARD_CONTENT LIKE '%50%'; -- 내용
+-- AND (BOARD_TITLE LIKE '%50%' OR BOARD_CONTENT LIKE '%50%'); -- 제목 + 내용
+AND MEMBER_NICK LIKE '%유저일%';
+
+		SELECT * FROM(
+		    SELECT ROWNUM RNUM, A.* FROM(
+		        SELECT BOARD_NO, BOARD_TITLE, MEMBER_NICK,
+		                TO_CHAR(CREATE_DT, 'YYYY-MM-DD') AS CREATE_DT,
+		                READ_COUNT
+		        FROM BOARD
+		        JOIN MEMBER USING(MEMBER_NO)
+		        WHERE BOARD_CD = ?
+		        AND BOARD_ST = 'N'
+                
+                -- condition
+		        ORDER BY BOARD_NO DESC
+		    ) A
+		)
+		WHERE RNUM BETWEEN ? AND ?
